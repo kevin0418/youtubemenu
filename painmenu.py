166950fb1d250d4087759d98c2af6a85d44eb50d
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2024-11-22 23:26:45 +1000

    add first time upload
    add first time upload

---
+++ painmenu.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.write("## 우환  고통을   :red[이기는 기슬] ") 
+
+st.markdown(
+"""    
+     [Video 보기 ](https://youtu.be/PyYXWM0QO_8)\n
+     [PDF Format 으로 보기  ](https://papago.naver.com/)\n 
+     
+     
+"""
+)  
+with st.sidebar:
+     
+     st.markdown(
+      """ 
+          
+       아래 를 선택 하시고 음미 해보세요
+                        
+          - [용서 ](https://youtu.be/Pt5_qFeGl3Q)
+          - [상상력 의 힘 ](https://youtu.be/4bSIVsGl7GM)
+
+                
+
+    """
+    )
+     
+
+    
+
+
---
+++ pdfviewer.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from streamlit_pdf_viewer import pdf_viewer
+# PDF 파일 경로
+pdf_file_path = "pain1.pdf"
+
+# PDF 뷰어 컴포넌트 사용
+pdf_viewer(pdf_file_path)
+
+
